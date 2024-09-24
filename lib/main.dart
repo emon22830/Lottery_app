12b0cce8fad6e99821a8f7739fabdeb0b7a9f9f8
emon22830 <mdemon22830@gmail.com>
2024-09-24 10:47:50 +0800

    Progress 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(LotteryApp());
@@ -12,8 +13,9 @@ class LotteryApp extends StatefulWidget {
 }
 
 class _LotteryAppState extends State<LotteryApp> {
+  Random random = Random();
 
-  int x =4;
+  int x = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -29,20 +31,47 @@ class _LotteryAppState extends State<LotteryApp> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Center(child: Text('Lottery winning number is $x')),
-         Container(
-           height: 250,
-           child: Column(
-             children: [
-
-             ],
-           )
-         )
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+                height: 250,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(.3),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.error_outline,
+                        size: 35,
+                        color: Colors.red,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        'Better Luck for next time, \nyour number is 4 try again',
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ))
           ],
         ),
         floatingActionButton: FloatingActionButton(
-            onPressed: (){
+          onPressed: () {
+            // print(random.nextInt(10).toString());
+            x = random.nextInt(100);
+            print(x);
+            setState(() {
 
-            },
+            });
+          },
           child: Icon(Icons.refresh),
         ),
       ),
